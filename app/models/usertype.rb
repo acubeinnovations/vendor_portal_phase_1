d415d8cb0b087b2d54d3356f322229b9aa076e9a
Nijo Joseph <nijojoseph@acube.co>
2015-02-12 16:02:44 +0530

    layout intgration

--- app/models/usertype.rb
+++ app/models/usertype.rb
@@ -1,5 +1,6 @@
 class Usertype
   include Mongoid::Document
+	include Mongoid::Timestamps::Created
   field :userrole, type: String
   field :description, type: String
   field :_id, type: String, default:->{ userrole.to_s.parameterize}
