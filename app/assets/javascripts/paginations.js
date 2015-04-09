5153799a9d3b25c50b9d802936219fc50da25cb3
Arun R <arunr@acube.co>
2015-04-09 19:29:00 +0530

    pagination

--- app/assets/javascripts/paginations.js
+++ app/assets/javascripts/paginations.js
@@ -1,11 +1,6 @@
-//$("ul").delegate("li.option", "click", function(){
 $('.pagination').delegate('.pagination a','click', function(event) { 
-//$('.pagination a').on('click', function(event) { 
-	
-	//alert("clicked");
-	 event.preventDefault();  
-  //$('.pagination').html('Data is loading...');  
-   $.get(this.href, null, null, 'script');  
+	event.preventDefault();  
+ $.get(this.href, null, null, 'script');  
    return false;  
  }); 
 
